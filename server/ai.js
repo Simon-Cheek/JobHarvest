0b2deb2379b271c00d374a38f22622f6afcdc7ac
Sasha Cheek <sasha@cheek.tv>
2024-10-26 14:45:53 -0600

    Organize server files

--- server/ai.js
+++ server/ai.js
@@ -1,11 +1,13 @@
-const { GoogleGenerativeAI } = require("@google/generative-ai");
-
+// env
 require("dotenv").config();
 const apiKey = process.env.API_KEY;
 
+// gemini (AI model)
+const { GoogleGenerativeAI } = require("@google/generative-ai");
 const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
+// test the AI
 const testAI = async () => {
   const prompt = "Write me a sentence no longer than 5 words.";
 
@@ -13,4 +15,5 @@ const testAI = async () => {
   console.log(result.response.text());
 };
 
+// export
 module.exports = { testAI };
--- server/index.js
+++ server/index.js
@@ -1,24 +1,19 @@
+// requirements
 const ai = require("./ai.js");
 
-// Import the express module
+// express setup
 const express = require("express");
-
-// Create an instance of an Express application
 const app = express();
-
-// Define the port number
 const PORT = 3000;
-
-// Middleware to parse JSON bodies
 app.use(express.json());
 
-// Sample route for the root path
+// root path
 app.get("/", async (req, res) => {
   res.send("Hello, World! Welcome to my Express app!");
   ai.testAI();
 });
 
-// Start the server
+// start server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
